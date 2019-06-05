f90a110bb9fe712780f5151e83fbd854c0338d3e
dasha-kop <testSoft-Logic@yandex.ru>
2019-06-05 16:42:41 +0700

    Добавление описания теста

--- src/test/java/StepDef.groovy
+++ src/test/java/StepDef.groovy
@@ -4,8 +4,19 @@ this.metaClass.mixin(cucumber.api.groovy.Hooks)
 this.metaClass.mixin(cucumber.api.groovy.EN)
 
 Дано(~/^При выборе части тела нужно ввести номер  согласно выданному списку |(\d+)|$/) { int arg1 ->
-    // Write code here that turns the phrase above into concrete actions
-    throw new PendingException()
+    BackHealth backHealth = new BackHealth()
+    HeadHealth headHealth = new HeadHealth()
+    Therapist therapist
+
+    headHealth.welcome()
+    switch (arg1) {
+        case 1:
+            therapist = new Therapist(headHealth)
+            break
+        case 2:
+            therapist = new Therapist(backHealth)
+            break
+    }
 }
 Когда(~/^Пользователь ввел правильное число |(\d+)|$/) { int arg1 ->
     // Write code here that turns the phrase above into concrete actions
