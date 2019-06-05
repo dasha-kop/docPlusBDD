524d3e74965a36787ad1824e5343875eea00222e
dasha-kop <testSoft-Logic@yandex.ru>
2019-06-05 16:44:32 +0700

    Добавление интерфейса IVirtualDoctor

---
+++ src/main/java/ru/dasha/kop/IVirtualDoctor.java
@@ -0,0 +1,17 @@
+package ru.dasha.kop;
+
+public interface IVirtualDoctor {
+
+    default void welcome() {
+        System.out.println("Здравствуйте, вас приветствует альфа версия ПО виртуальный доктор \"DocPlus\" ");
+    }
+
+    void printTitle();
+
+    void printQuestions();
+
+    void diagnosis(int number);
+
+    int countQuestions();
+
+}
