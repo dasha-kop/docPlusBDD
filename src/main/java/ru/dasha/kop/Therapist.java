f6018a671eda3ece5aecce89c696822618907153
dasha-kop <testSoft-Logic@yandex.ru>
2019-06-05 16:46:50 +0700

    Добавление класса Доктора

---
+++ src/main/java/ru/dasha/kop/Therapist.java
@@ -0,0 +1,22 @@
+package ru.dasha.kop;
+
+public class Therapist {
+    private IVirtualDoctor virtualDoctor;
+
+    public Therapist(IVirtualDoctor virtualDoctor) {
+        this.virtualDoctor = virtualDoctor;
+    }
+
+    public void printQuestions(){
+        virtualDoctor.printQuestions();
+    }
+
+    public int getCount(){
+        return virtualDoctor.countQuestions();
+    }
+
+    public void getAnswer(int num){
+        virtualDoctor.diagnosis(num);
+    }
+
+}
